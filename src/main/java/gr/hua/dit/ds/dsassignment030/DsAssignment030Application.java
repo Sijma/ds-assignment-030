af10122486af11daaaa510aa970b52392a95c446
Sijma <johnbalasis@yahoo.gr>
2020-12-26 21:52:26 +0200

    Initial commit

---
+++ src/main/java/gr/hua/dit/ds/dsassignment030/DsAssignment030Application.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.dsassignment030;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAssignment030Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAssignment030Application.class, args);
+	}
+
+}
