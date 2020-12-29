e00d10172339b6ec66e43584a83bc0b51919c1cf
Sijma <johnbalasis@yahoo.gr>
2020-12-29 15:19:30 +0200

    Basic Security Implementation

---
+++ src/main/java/gr/hua/dit/ds/dsassignment030/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package gr.hua.dit.ds.dsassignment030.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer
+{
+
+}
